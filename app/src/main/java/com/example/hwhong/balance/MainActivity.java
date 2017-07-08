7488c00ee85e1c3699a57fe4f4cec96b7da2512e
Hwa Chien Hong <hwhong1129@gmail.com>
2017-07-08 21:56:05 +0800

    Done with the main activity screen

--- app/src/main/java/com/example/hwhong/balance/MainActivity.java
+++ app/src/main/java/com/example/hwhong/balance/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.hwhong.balance;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Disable the Title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // for full screen view
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
     }
 }
