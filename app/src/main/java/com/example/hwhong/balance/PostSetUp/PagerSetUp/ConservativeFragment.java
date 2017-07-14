302314d039194c3a83ea173c1d846cc6004669ae
Hwa Chien Hong <hwhong1129@gmail.com>
2017-07-14 11:24:17 +0800

    Spark implemented along with segmented radio buttons

--- app/src/main/java/com/example/hwhong/balance/PostSetUp/PagerSetUp/ConservativeFragment.java
+++ app/src/main/java/com/example/hwhong/balance/PostSetUp/PagerSetUp/ConservativeFragment.java
@@ -8,14 +8,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.example.hwhong.balance.PostSetUp.PortfolioUIComponents.RandomizedAdapter;
 import com.example.hwhong.balance.R;
+import com.robinhood.spark.SparkView;
 
 import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import info.hoang8f.android.segmented.SegmentedGroup;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,20 +45,22 @@ public class ConservativeFragment extends Fragment {
     // View Elements
     @BindView(R.id.conservative_tv)            TextView heading;
     @BindView(R.id.heading_under_conservative) TextView heading_under;
+    @BindView(R.id.conservative_sparkview)     SparkView sparkView;
+    private RandomizedAdapter adapter;
+
+    // Radio Button Views
+    @BindView(R.id.one_con)                     RadioButton one;
+    @BindView(R.id.two_con)                     RadioButton two;
+    @BindView(R.id.three_con)                   RadioButton three;
+    @BindView(R.id.four_con)                    RadioButton four;
+    @BindView(R.id.five_con)                    RadioButton five;
+
+    @BindView(R.id.seg_con)                     SegmentedGroup segmentedGroup;
 
     public ConservativeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConservativeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ConservativeFragment newInstance(String param1, String param2) {
         ConservativeFragment fragment = new ConservativeFragment();
         Bundle args = new Bundle();
@@ -83,17 +90,25 @@ public class ConservativeFragment extends Fragment {
         heading.setTypeface(dinot);
         heading_under.setTypeface(dinot);
 
+        adapter = new RandomizedAdapter();
+        sparkView.setAdapter(adapter);
+
+        //segmentedGroup.setTintColor(getResources().getColor(R.color.yellow), getResources().getColor(R.color.white));
 
 
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    @OnClick(R.id.one_con)
+    public void randomize1() {adapter.randomize();}
+    @OnClick(R.id.two_con)
+    public void randomize2() {adapter.randomize();}
+    @OnClick(R.id.three_con)
+    public void randomize3() {adapter.randomize();}
+    @OnClick(R.id.four_con)
+    public void randomize4() {adapter.randomize();}
+    @OnClick(R.id.five_con)
+    public void randomize5() {adapter.randomize();}
 
     /**
      * This interface must be implemented by activities that contain this
---
+++ app/src/main/java/com/example/hwhong/balance/PostSetUp/PortfolioUIComponents/RandomizedAdapter.java
@@ -0,0 +1,42 @@
+package com.example.hwhong.balance.PostSetUp.PortfolioUIComponents;
+
+import com.robinhood.spark.SparkAdapter;
+
+import java.util.Random;
+
+/**
+ * Created by hwhong on 7/14/17.
+ */
+
+public class RandomizedAdapter extends SparkAdapter {
+    private final float[] yData;
+    private final Random random;
+
+    public RandomizedAdapter() {
+        random = new Random();
+        yData = new float[50];
+        randomize();
+    }
+
+    public void randomize() {
+        for (int i = 0, count = yData.length; i < count; i++) {
+            yData[i] = random.nextFloat();
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return yData.length;
+    }
+
+    @Override
+    public Object getItem(int index) {
+        return yData[index];
+    }
+
+    @Override
+    public float getY(int index) {
+        return yData[index];
+    }
+}
