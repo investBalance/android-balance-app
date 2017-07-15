ca1eedcb6306a0d6dc9718d5f72e638396e1843b
Hwa Chien Hong <hwhong1129@gmail.com>
2017-07-15 10:48:24 +0800

    done with aggressive, lacking major icons

--- app/src/main/java/com/example/hwhong/balance/PostSetUp/PagerSetUp/AggressiveFragment.java
+++ app/src/main/java/com/example/hwhong/balance/PostSetUp/PagerSetUp/AggressiveFragment.java
@@ -1,14 +1,32 @@
 package com.example.hwhong.balance.PostSetUp.PagerSetUp;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.example.hwhong.balance.PostSetUp.PortfolioUIComponents.RandomizedAdapter;
 import com.example.hwhong.balance.R;
+import com.github.lzyzsd.circleprogress.DonutProgress;
+import com.robinhood.spark.SparkView;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import info.hoang8f.android.segmented.SegmentedGroup;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +47,40 @@ public class AggressiveFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    // View Elements
+    @BindView(R.id.aggressive_tv)
+    TextView heading;
+    @BindView(R.id.heading_under_aggressive)      TextView heading_under;
+    @BindView(R.id.aggressive_sparkview)
+    SparkView sparkView;
+    private RandomizedAdapter adapter;
+
+    // Radio Button Views
+    @BindView(R.id.one_agg)                     RadioButton one;
+    @BindView(R.id.two_agg)                     RadioButton two;
+    @BindView(R.id.three_agg)                   RadioButton three;
+    @BindView(R.id.four_agg)                    RadioButton four;
+    @BindView(R.id.five_agg)                    RadioButton five;
+
+    @BindView(R.id.seg_agg)                     SegmentedGroup segmentedGroup;
+    @BindView(R.id.bar1_aggressive)             ProgressBar bar1;
+    @BindView(R.id.bar2_aggressive)             ProgressBar bar2;
+
+    @BindView(R.id.bar1_text_aggressive)        TextView bar1_text;
+    @BindView(R.id.bar2_text_aggressive)        TextView bar2_text;
+    @BindView(R.id.bar1_value_aggressive)       TextView bar1_value;
+    @BindView(R.id.bar2_value_aggressive)       TextView bar2_value;
+
+    @BindView(R.id.pie1_text_agg)               TextView pie1_text_agg;
+    @BindView(R.id.pie2_text_agg)               TextView pie2_text_agg;
+    @BindView(R.id.pie3_text_agg)               TextView pie3_text_agg;
+    @BindView(R.id.pie4_text_agg)               TextView pie4_text_agg;
+
+    @BindView(R.id.donut_animal_agg)            DonutProgress donut_animal;
+    @BindView(R.id.donut_environment_agg)       DonutProgress donut_environment;
+    @BindView(R.id.donut_sustainable_agg)       DonutProgress donut_sustainable;
+    @BindView(R.id.donut_peace_agg)             DonutProgress donut_peace;
 
     public AggressiveFragment() {
         // Required empty public constructor
@@ -52,11 +104,82 @@ public class AggressiveFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aggressive, container, false);
+        View view = inflater.inflate(R.layout.fragment_aggressive, container, false);
+        ButterKnife.bind(this, view);
+
+        Typeface dinot = Typeface.createFromAsset(getActivity().getAssets(), "fonts/DINOT-Regular.ttf");
+        heading.setTypeface(dinot);
+        heading_under.setTypeface(dinot);
+
+        adapter = new RandomizedAdapter();
+        sparkView.setAdapter(adapter);
+
+        //segmentedGroup.setTintColor(getResources().getColor(R.color.yellow), getResources().getColor(R.color.white));
+        bar1.setMax(100);
+        bar1.setVisibility(View.VISIBLE);
+        bar1.setProgress(35);
+        bar1.setProgressTintList(ColorStateList.valueOf(Color.GREEN));
+
+        bar2.setMax(100);
+        bar1.setVisibility(View.VISIBLE);
+        bar2.setProgress(75);
+        bar2.setProgressTintList(ColorStateList.valueOf(Color.GREEN));
+
+        bar1_text.setTypeface(dinot); bar2_text.setTypeface(dinot);
+        bar1_value.setTypeface(dinot); bar2_value.setTypeface(dinot);
+
+        donutSetUp();
+
+        return view;
+    }
+
+    private void donutSetUp() {
+        Random random = new Random();
+        int i1 = random.nextInt(101);
+        donut_animal.setProgress((float)i1);
+        pie1_text_agg.setText(i1 + "%");
+
+        int i2 = random.nextInt(101);
+        donut_environment.setProgress((float)i2);
+        pie2_text_agg.setText(i2 + "%");
+
+        int i3 = random.nextInt(101);
+        donut_peace.setProgress((float)i3);
+        pie3_text_agg.setText(i3 + "%");
+
+        int i4 = random.nextInt(101);
+        donut_sustainable.setProgress((float)i4);
+        pie4_text_agg.setText(i4 + "%");
+    }
+
+    @OnClick(R.id.one_agg)
+    public void randomize1() {adapter.randomize(); randomizeTexts(); donutSetUp();}
+    @OnClick(R.id.two_agg)
+    public void randomize2() {adapter.randomize(); randomizeTexts(); donutSetUp();}
+    @OnClick(R.id.three_agg)
+    public void randomize3() {adapter.randomize(); randomizeTexts(); donutSetUp();}
+    @OnClick(R.id.four_agg)
+    public void randomize4() {adapter.randomize(); randomizeTexts(); donutSetUp();}
+    @OnClick(R.id.five_agg)
+    public void randomize5() {adapter.randomize(); randomizeTexts(); donutSetUp();}
+
+    private void randomizeTexts() {
+        Random random = new Random();
+
+        // Value texts and progress
+        int value = random.nextInt(750);
+        bar1_value.setText("$" + value);
+        bar1.setProgress(value/10);
+
+        // Projected texts and progress
+        int projected = random.nextInt(1001);
+        while(value > projected) projected = random.nextInt(1001);
+        bar2.setProgress(projected/10);
+        bar2_value.setText("$"+projected);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
