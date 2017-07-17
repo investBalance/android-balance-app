5096b3a1b51c5485648e11acdc93a3f934e7f2d5
Hwa Chien Hong <hwhong1129@gmail.com>
2017-07-17 12:45:05 +0800

    Main modified

--- app/src/main/java/com/example/hwhong/balance/MainActivity.java
+++ app/src/main/java/com/example/hwhong/balance/MainActivity.java
@@ -44,10 +44,10 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.createAccountBut)
     public void createAccount() {
-        //Intent intent = new Intent(getApplicationContext(), CreditCardActivity.class);
-        //startActivity(intent);
-
-        Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
+        Intent intent = new Intent(getApplicationContext(), EmailActivity.class);
         startActivity(intent);
+
+        //Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
+        //startActivity(intent);
     }
 }
