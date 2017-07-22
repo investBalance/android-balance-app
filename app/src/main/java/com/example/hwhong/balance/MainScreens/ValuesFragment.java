a91727d7a5174ff67a73c3ecc3e5fd26da1e463b
Hwa Chien Hong <hwhong1129@gmail.com>
2017-07-22 12:10:08 +0800

    code cleanup, nav bars customize

--- app/src/main/java/com/example/hwhong/balance/MainScreens/ValuesFragment.java
+++ app/src/main/java/com/example/hwhong/balance/MainScreens/ValuesFragment.java
@@ -13,36 +13,17 @@ import com.example.hwhong.balance.R;
 
 import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ValuesFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ValuesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ValuesFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private ValuesFragment.OnFragmentInteractionListener mListener;
-
 
     public ValuesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ValuesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ValuesFragment newInstance(String param1, String param2) {
         ValuesFragment fragment = new ValuesFragment();
         Bundle args = new Bundle();
@@ -73,21 +54,5 @@ public class ValuesFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
