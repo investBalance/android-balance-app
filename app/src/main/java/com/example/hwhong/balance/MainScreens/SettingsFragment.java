11769e4973abab6dfab5a04c2193fa3645c87bc0
Hwa Chien Hong <hwhong1129@gmail.com>
2017-08-03 10:24:05 +0900

    cleanup

--- app/src/main/java/com/example/hwhong/balance/MainScreens/SettingsFragment.java
+++ app/src/main/java/com/example/hwhong/balance/MainScreens/SettingsFragment.java
@@ -30,10 +30,6 @@ public class SettingsFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     // View elements
